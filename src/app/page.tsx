c9632c77548052ec7cfba541a63ca779d543fd97
rstubryan <restubumirr@gmail.com>
2025-05-20 10:03:52 +0700

    feat: update layout and home page to showcase portfolio projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,80 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <div>
+        <h1 className={'font-bold'}>Portfolio, Click the link to see the project</h1>
+        <ul className={"list-disc list-inside"}>
+          <Link href={"https://rstubryan.github.io/dpop/"}>
+            <li className="hover:underline">Dzikir Pop (2023)</li>
+          </Link>
+          <Link href={"https://www.indonesiasatuhati.id/"}>
+            <li className="hover:underline">Pengembangan Fitur Psikotes Homeschooling Permata Hati dengan CodeIgniter dan Bootstrap (2023)</li>
+          </Link>
+          <Link href={"https://hmti.nusaputra.ac.id/"}>
+            <li className="hover:underline">Pengembangan Laman Website Himpunan Mahasiswa Teknik Informatika dengan Laravel dan Tailwind CSS (2023)</li>
+          </Link>
+          <li>Sistem Kunjungan Wali Santri Santri Pondok Pesantren As-Salam Sukabumi (2023) (Private)</li>
+          <Link href={"https://rstubryan.github.io/kkn/"}>
+            <li className="hover:underline">Landing Pages Website KKN Desa Wangunsari (2023)</li>
+          </Link>
+          <Link href={"https://rstubryan.github.io/pariwisata/"}>
+            <li className="hover:underline">Landing Pages Curug Cierang (2023)</li>
+          </Link>
+          <Link href={"https://github.com/rstubryan/laravel-lawaklu"}>
+            <li className="hover:underline">Lawaklu : Meme Website (2023) (Repository)</li>
+          </Link>
+          <Link href={"https://github.com/rstubryan/KELOMPOK5-FE6"}>
+            <li className="hover:underline">Landing Pages UMKM KW Vape Store (2024) (MSIB Batch 6 : Gamelab ID)</li>
+          </Link>
+          <Link href={"https://rstubryan.github.io/castro/"}>
+            <li className="hover:underline">Castro : Podcast Horror Website (2024)</li>
+          </Link>
+          <Link href={"https://github.com/GamelabAhmad/Project20-4-ForumDiskusiApp-Frontend"}>
+            <li className="hover:underline">Twenties : Forum Discussion App (2024) (MSIB Batch 6 : Gamelab ID)</li>
+          </Link>
+          <li>Sanskara Karya Conference (2024) (Internship)</li>
+          <Link href={"https://github.com/rstubryan/dts-todo-app-expo"}>
+            <li className="hover:underline">Todo App with React Native Expo (2024) (Repository)</li>
+          </Link>
+          <Link href={"https://github.com/rstubryan/dts-weather-app-expo"}>
+            <li className="hover:underline">Weather App with React Native Expo (2024) (Repository)</li>
+          </Link>
+          <Link href={"https://github.com/rstubryan/dts-movie-app-progate-expo"}>
+            <li className="hover:underline">TMDB Movie App with React Native Expo (2024) (Repository)</li>
+          </Link>
+          <Link href={"https://github.com/rstubryan/pomodoro-fe"}>
+            <li className="hover:underline">Pomodoro App (2024) (Repository)</li>
+          </Link>
+        </ul>
+        <h1 className={'font-bold'}>Ongoing Project and Production Ready</h1>
+        <Link href={"https://dev.v2.telkom.ideaboxapp.com"}>
+          <li className="hover:underline">Ideabox : Idea Management System (2024) (MSIB Batch 7 : Digital Amoeba)</li>
+        </Link>
+        <Link href={"https://sineasmov.com"}>
+          <li className="hover:underline">SineasMov : OTT Platform (2024 - present) (Part Time Mentor)</li>
+        </Link>
+        <Link href={"http://sineasmov.com:81/dashboard"}>
+          <li className="hover:underline">Backoffice SineasMov (2024 - present) (Part Time Mentor)</li>
+        </Link>
+        <Link href={"http://82.112.226.151:8084/dashboard"}>
+          <li className="hover:underline">HRIS SineasMov (2024 - present) (Part Time Mentor)</li>
+        </Link>
+        <Link href={"https://hris-payroll.zeabur.app/dashboard"}>
+          <li className="hover:underline">HR Payroll : Payroll System (Apr 2025 - present) (Part Time Mentor)</li>
+        </Link>
+        <Link href={"https://flexo-fo.zeabur.app"}>
+          <li className="hover:underline">Flexo : Sweeping Order System (Apr 2025 - present) (Freelance)</li>
+        </Link>
+        <Link href={"https://counseling-npu.zeabur.app"}>
+          <li className="hover:underline">E-Counseling NPU : Counseling System and Live Chat (Apr 2025 - present) (Freelance)</li>
+        </Link>
+        <Link href={"https://dapur-abc.zeabur.app"}>
+          <li className="hover:underline">Dapur ABC : E-Commerce System (Apr 2025 - present) (Freelance)</li>
+        </Link>
+        <Link href={"https://tascrum.zeabur.app/login"}>
+            <li className="hover:underline">Tascrum : Scrum Task Management System (Apr 2025 - present) (Thesis)</li>
+        </Link>
+      </div>
   );
 }
